73fa82f2c2ab4ed0b3e98eb64422f64630a6e029
akshatpapnoi <akshatpapnoi1110@gmail.com>
2019-11-05 03:29:26 +0530

    first commit

--- parkingsystem/urls.py
+++ parkingsystem/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from main import views as main_view
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', main_view.main, name='main'),
 ]
